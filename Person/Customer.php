403d24959e490a1246ddbc91b2a06a4b8c712ea6
acidvertigo <ldanzelmo@gmail.com>
2017-09-21 17:49:01 +0200

    Saving

--- Person/Customer.php
+++ Person/Customer.php
@@ -10,4 +10,24 @@ class Customer extends Person;
 	{
 		$this->id = $id;
 	}
+	
+	public function getName()
+	{
+		return $this->name;
+	}
+	
+	public function setName($name)
+	{
+        $this->name = $name;
+	}
+	
+	public function getSurName()
+	{
+		return $this->surname;
+	}
+	
+	public function setSurName($surname)
+	{
+        $this->surname = $surname;
+	}
 }
