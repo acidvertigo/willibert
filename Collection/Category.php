492e5afe1f99ca5a672cc9ab87f635830d0d9f03
acidvertigo <ldanzelmo@gmail.com>
2017-09-21 23:07:01 +0200

    Saving

--- Collection/Category.php
+++ Collection/Category.php
@@ -1,10 +1,58 @@
 <?php
 
-use Willibert\Abstraction;
+use Willibert\Abstraction\Collection;
 
 namespace Willibert\Collection;
 
 class Category extends Collection
 {
-	
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->items);
+    }
+    
+    public function get($key)
+    {
+    	
+    }
+    
+    public function getAll()
+    {
+    	
+    }
+    
+    public function add($key, $value)
+    {
+    	
+    }
+    
+    public function remove($key)
+    {
+    	
+    }
+
+    public function contains($value)
+    {
+    	
+    }
+    
+    public function push($value)
+    {
+    	
+    }
+    
+    public function prepend($value)
+    {
+    	
+    }
+
+    public function sort(\Closure $callback)
+    {
+    	
+    }
 }
