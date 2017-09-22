5045bf19483df59322dbdbfc0c7626d10ebca52f
acidvertigo <ldanzelmo@gmail.com>
2017-09-22 22:30:17 +0200

    Saving

--- Collection/Category.php
+++ Collection/Category.php
@@ -6,31 +6,7 @@ namespace Willibert\Collection;
 
 class Category extends Collection
 {
-    public function count()
-    {
-        return count($this->items);
-    }
-
-    public function getIterator()
-    {
-        return new \ArrayIterator($this->items);
-    }
-    
-    public function get($key)
-    {
-    	return $this->items[$key];
-    }
-    
-    public function getAll()
-    {
-    	return $this->items;
-    }
-    
-    public function add($key, $value)
-    {
-    	$this->items[$key] = $value;
-    }
-    
+   
     public function remove($key)
     {
     	unset($this->items[$key]);
