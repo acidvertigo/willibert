a931fe5819f0122962a0073920c72c1e2a1818b5
acidvertigo <ldanzelmo@gmail.com>
2017-09-22 22:24:46 +0200

    Saving

--- Abstraction/Item.php
+++ Abstraction/Item.php
@@ -14,6 +14,6 @@ abstract class Item
 		return $this->id;
 	}
 	
-	/* map one array to obkect properties */
+	/* map one array to object properties */
 	abstract public function map(array $data);
 }
