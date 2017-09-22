042ce652a643a75003a40ffc4dd64b65eae87894
acidvertigo <ldanzelmo@gmail.com>
2017-09-22 22:31:04 +0200

    Saving

--- Collection/Category.php
+++ Collection/Category.php
@@ -7,28 +7,5 @@ namespace Willibert\Collection;
 class Category extends Collection
 {
    
-    public function remove($key)
-    {
-    	unset($this->items[$key]);
-    }
-
-    public function contains($value)
-    {
-    	return in_array($this->items, $value);
-    }
-    
-    public function push($value)
-    {
-    	
-    }
-    
-    public function prepend($value)
-    {
-    	
-    }
-
-    public function sort(\Closure $callback)
-    {
-    	uasort($this->items, $callback);
-    }
+   
 }
