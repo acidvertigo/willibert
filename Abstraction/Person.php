9a1d4f7b87f1417811f5b3ebbbc3f84f3dae74a8
acidvertigo <ldanzelmo@gmail.com>
2017-09-25 00:52:11 +0200

    Saving

--- Abstraction/Person.php
+++ Abstraction/Person.php
@@ -7,24 +7,13 @@ namespace Willibert\Abstraction;
 
 abstract class Person
 {
-	protected $id;
-	protected $name;
-	protected $surname;
-	protected $address;
-	protected $dob;
-	protected $email;
-	protected $telephone;
-	protected $fax;
-	protected $password;
+
+    $protected $id;
 
 	public function getId()
 	{
 		return $this->id;
 	}
-	
-	abstract public function getName();
-	
-	abstract public function getSurname();
 	
 	/* map one array to object properties */
 	abstract public function map(array $data);
