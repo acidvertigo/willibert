da75b4759d57c4ff1d3d9a80fa451bd85fb0efa5
acidvertigo <ldanzelmo@gmail.com>
2017-09-25 00:50:51 +0200

    Saving

--- Person/Customer.php
+++ Person/Customer.php
@@ -50,53 +50,8 @@ class Customer extends Person;
 		}
 	}
 	
-	public function getDob()
+	public function getProperty($property)
 	{
-		return $this->dob;
-	}
-	
-	public function setDob($dob)
-	{
-	    $this->dob = $dob;	
-	}
-	
-	public function getEmail()
-	{
-		return $this->email;
-	}
-	
-	public function setEmail($email)
-	{
-		$this->email = $email;
-	}
-	
-	public function getPhone()
-	{
-		return $this->telephone;
-	}
-	
-	public function setPhone($telephone)
-	{
-		$this->phone = $telephone;
-	}
-	
-	public function getFax()
-	{
-		return $this->fax;
-	}
-	
-	public function setFax($fax)
-	{
-		$this->fax = $fax;
-	}
-	
-	public function getPassword()
-	{
-		return $this->password;
-	}
-	
-	public funcion setPassword($password)
-	{
-		$this->password = $password;
+		return $this->$property;
 	}
 }
